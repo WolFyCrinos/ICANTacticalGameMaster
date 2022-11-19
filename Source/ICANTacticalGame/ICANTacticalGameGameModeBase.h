284a2fe330016f179024c6756e7c4347da8cb893
François MARTEEL <francois59123@hotmail.fr>
2022-11-19 18:28:19 +0100

    First Commit
    Let's Go !

---
+++ Source/ICANTacticalGame/ICANTacticalGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ICANTacticalGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ICANTACTICALGAME_API AICANTacticalGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
